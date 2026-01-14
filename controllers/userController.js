649a9262fd7ea48267e57e760a3581e26bac1a65
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-01-14 19:23:10 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const AWS=require("aws-sdk")
 exports.getUploadURL = async (req, res) => {
   try {
     const { uploadURL, key } = generateUploadURL();
+    console.log(key);
     const imageUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
     res.json({ uploadURL, imageUrl });
   } catch (err) {
